0291eef88223da5a0c4799773444c6ae00f7342f
Jonathas Rodrigues <jonathasrc96@gmail.com>
2021-05-31 19:18:23 -0300

    OrangeApllication.java

--- src/main/java/com/orange/OrangeApplication.java
+++ src/main/java/com/orange/OrangeApplication.java
@@ -1,13 +1,20 @@
 package com.orange;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrangeApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(new Locale("pt", "BR"));
 		SpringApplication.run(OrangeApplication.class, args);
+		
+
 	}
 
 }
