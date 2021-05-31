e7b7b9c95cb5386f8074acc08784320e390c05f2
Jonathas Rodrigues <jonathasrc96@gmail.com>
2021-05-31 19:17:45 -0300

    Testconfig

--- src/main/java/com/orange/config/TestConfig.java
+++ src/main/java/com/orange/config/TestConfig.java
@@ -8,14 +8,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.orange.entities.User;
+import com.orange.entities.Vehicle;
 import com.orange.repository.UserRepository;
+import com.orange.repository.VehicleRepository;
+import com.orange.utils.GetDayOfWeek;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
+	
+	@Autowired 
+	private VehicleRepository vehiclesRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,9 +30,21 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Jonathas", "Jonathas@gmail.com","111111111-11","24-05-2000");
 		User u2 = new User(null, "Jonathas", "Jonathas@gmail.com","111111111-11","24-05-2000");
 		
-		System.out.println(u1.equals(u2));
+		Vehicle v1 = new Vehicle(null, "VW - VolksWagen","AMAROK High.CD 2.0 16V TDI 4x4 Dies. Aut", "2014");
+		Vehicle v2 = new Vehicle(null, "VW - VolksWagen","AMAROK High.CD 2.0 16V TDI 4x4 Dies. Aut", "2014");
 		
-		repository.saveAll(Arrays.asList(u1, u2));
+		
+
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		vehiclesRepository.saveAll(Arrays.asList(v1, v2));
+		
+		u1.getVehicles().add(v1);
+		
+		userRepository.save(u1);
+		
+		
+		
 	}
 
 }
